eefbe971d1191bcd7a9a35bdb9731fcf0fdcf30e
loicngr <pro.nogierloic@gmail.com>
2025-06-06 15:17:55 +0200

    🔖 chore(build): bump versionCode to 3 and versionName to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "fr.loicnogier.mystreamlogs"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
