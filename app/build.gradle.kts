4ea4a066193f6beef867a8c62bc95f52b283a6fc
loicngr <pro.nogierloic@gmail.com>
2025-07-05 13:32:49 +0200

    feature/#8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "fr.loicnogier.mystreamlogs"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
