81702215a4817800f9f0081919b8606a5d36500a
loicngr <pro.nogierloic@gmail.com>
2025-07-06 11:06:39 +0200

    🔖 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "fr.loicnogier.mystreamlogs"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4.1"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
